c7dc39373f351a4f232a608b77e711827bb1b571
arthurf-lab <arthurhastros@gmail.com>
2025-11-13 10:40:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.header('Vamos fazer algo com interatividade')
 
 n = st.number_input('Entre com um numero')
 
-st.write(f'O número que você escolheu ao quadrado é(n**2)')
+st.write(f'O número que você escolheu ao quadrado é (n**2)')
